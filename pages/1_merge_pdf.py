f0d25dbea8d17255dd8503e5015d04a561d92886
Amit A Chengode <amitachengode@gmail.com>
2026-07-31 17:34:13 +0530

    style: updated style for merge files page

--- pages/1_merge_pdf.py
+++ pages/1_merge_pdf.py
@@ -1,20 +1,26 @@
 import streamlit as st
 from pdf_processor import merge_pdf
 
-
 st.set_page_config(page_title="PDF Merger", page_icon="📄", layout='wide')
 
 st.title("PDF Merger")
 st.write("Upload multiple PDF files to merge them into one.")
 
-upload_section, download_section = st.columns(2,)
+upload_section, export_section = st.columns([3, 1],)
 
-uploaded_files = st.file_uploader("Upload PDF files", type=["pdf"], accept_multiple_files=True, width=400)
+with upload_section:
+    st.subheader("Upload")
+    uploaded_files = st.file_uploader(label="", type=["pdf"], accept_multiple_files=True, label_visibility="collapsed")
 
-if uploaded_files:
-    if st.button("Merge PDFs", width=400):
+with export_section:
+    st.subheader("Export")
+    filename = st.text_input("output file name", value="merged_document.pdf")
 
-        with st.spinner("Merging PDFs..."):
-            merged_pdf = merge_pdf(uploaded_files)
-            
-        st.download_button("Download Merged PDF", merged_pdf, "merged.pdf", "application/pdf", width=400)
+    if len(uploaded_files)>=2:
+        if st.button("Merge files", use_container_width=True):
+            with st.spinner():
+                merged_pdf= merge_pdf(uploaded_files)
+            st.download_button("Export", data=merged_pdf, file_name=filename, mime="application/pdf", use_container_width=True)
+    else:
+        st.warning("Upload at least two files.")
+
