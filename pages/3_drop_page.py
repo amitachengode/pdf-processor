6fedb81e043cdc61d16d6c2a357d536f7af5a0c6
Amit A Chengode <amitachengode@gmail.com>
2026-07-31 19:10:21 +0530

    style: updated style for drop page page

--- pages/3_drop_page.py
+++ pages/3_drop_page.py
@@ -7,32 +7,22 @@ st.set_page_config(page_title="Page Dropper", page_icon="📄", layout='wide')
 st.title("Page Dropper")
 st.write("Upload a PDF file to drop pages from it.")
 
-uploaded_file = st.file_uploader("Upload PDF file", type=["pdf"], width=400, accept_multiple_files=False)
+upload_section, export_section = st.columns([3, 1],)
 
-if uploaded_file:
-    
-    total_pages = len(pypdf.PdfReader(uploaded_file).pages)
-    st.info(f"Document has {total_pages} pages.", width=400)
-    
-    page_input = st.text_input("Select pages to drop (e.g., 1, 3, 5-10)", placeholder="1, 3, 5-10", width=400)
-    
-    if st.button("Drop Pages", width=400):
-        if not page_input:
-            st.warning("Please enter the pages you want to drop.", width=400)
-        else:
-            with st.spinner("Dropping pages..."):
-                
+with upload_section:
+    st.subheader("Upload")
+    uploaded_files = st.file_uploader(label="", type=["pdf"], accept_multiple_files=False, label_visibility="collapsed")
+
+with export_section:
+    st.subheader("Export")
+    page_input = st.text_input("Select pages to drop (e.g., 1, 3, 5-10)", placeholder="1, 3, 5-10")
+    filename = st.text_input("output file name", value="dropped_document.pdf")
+    if page_input:
+        if st.button("Drop page", use_container_width=True):
+            with st.spinner():
+                total_pages = len(pypdf.PdfReader(uploaded_files).pages)
                 page_list = parse_page_ranges(page_input, total_pages)
-                
-                if not page_list:
-                    st.error("Invalid format. Please use numbers and dashes (e.g., 1, 3, 5-10).", width=400)
-                else:
-                    dropped_pdf = drop_pages_pdf(uploaded_file, page_list)
-                    
-                    st.download_button(
-                        label="Download Dropped PDF", 
-                        data=dropped_pdf, 
-                        file_name="dropped.pdf", 
-                        mime="application/pdf",
-                        width=400
-                    )
+                dropped_pdf = drop_pages_pdf(uploaded_files, page_list)
+            st.download_button(label="Export", data=dropped_pdf, file_name=filename, mime="application/pdf", use_container_width=True)
+    else:
+        st.warning("Enter the range in proper format.")
